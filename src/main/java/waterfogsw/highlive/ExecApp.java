6b74574569502ab269bb55fb359b706aa20f9b3c
San Kim <waterfog9580@gmail.com>
2021-11-15 19:09:51 +0900

    update

--- src/main/java/waterfogsw/highlive/ExecApp.java
+++ src/main/java/waterfogsw/highlive/ExecApp.java
@@ -1,26 +1,38 @@
 package waterfogsw.highlive;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ExecApp {
 
-    private static final String crawlerPath = "./pythonProgram/crawler.py";
+    private static final String crawlerPath = "./src/main/java/waterfogsw/highlive/pythonProgram/crawler.py";
 
-    public void byRuntime(String[] command) {
-//        Runtime run;
-    }
-    private void printStream(Process process) throws IOException, InterruptedException {
-        try (InputStream psout = process.getInputStream()) {
-            copy(psout, System.out);
-        }
-    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        List<String> command = new ArrayList<>();
+        command.add("python3");
+        command.add(crawlerPath);
+        command.add("1167834289");
 
-    public void copy(InputStream input, OutputStream output) throws IOException {
-        byte[] buffer = new byte[1024];
-        int n = 0;
-        while ((n = input.read(buffer)) != -1) {
-            output.write(buffer, 0, n);
+        // Exec Crawler
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        Process process = processBuilder.start();
+
+        process.waitFor();
+
+        InputStream psout = process.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(psout));
+
+        String line;
+        StringBuilder output = new StringBuilder();
+        while ((line = br.readLine()) != null) {
+            output.append(line);
         }
+        br.close();
+
+        System.out.println(output.toString());
     }
 }
