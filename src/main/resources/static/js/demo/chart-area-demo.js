8bf654140f2e93cbb50b79725beb98a87da32e7f
rilac1 <rilac1@naver.com>
2021-11-17 10:37:40 +0900

    getJSON 수정

--- src/main/resources/static/js/demo/chart-area-demo.js
+++ src/main/resources/static/js/demo/chart-area-demo.js
@@ -30,7 +30,7 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 // getData
 chartLabels = [];
 chartData = [];
-$.getJSON("localhost:8080/python/Data/raw_data/1204174820.json", function(data) {
+$.getJSON("localhost:8080/python/Data/result_data/1204174820.json", function(data) {
 	$.each(data, function(key, value){
         chartLabels.push(value[0]);
         chartData.push(parseFloat(value[1]));
@@ -42,7 +42,7 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    chartLabels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
       label: "Earnings",
       lineTension: 0.3,
@@ -56,7 +56,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      chartData: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
     }],
   },
   options: {
