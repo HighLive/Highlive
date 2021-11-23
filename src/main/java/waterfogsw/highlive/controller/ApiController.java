840fba35d3cd3beca4382ae05859f6f6316e25f7
rilac1 <rilac1@naver.com>
2021-11-23 11:23:02 +0900

    api 반환 json 변경 (raw to result)

--- src/main/java/waterfogsw/highlive/controller/ApiController.java
+++ src/main/java/waterfogsw/highlive/controller/ApiController.java
@@ -18,7 +18,7 @@ public class ApiController {
     @RequestMapping(value = "/api/json/{id}", method = RequestMethod.GET)
     public String getJsonFile(@PathVariable("id") String videoId) throws IOException {
 
-        File resource = new File("./python/Data/raw_data/"+ videoId + ".json");
+        File resource = new File("./python/Data/result_data/"+ videoId + ".json");
 
         return new String(Files.readAllBytes(resource.toPath()));
     }
