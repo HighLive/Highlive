7082f3e7afc076a0dd5f6795255ce35f96183336
San Kim <waterfog9580@gmail.com>
2021-11-24 11:24:40 +0900

    `python/Data/` json파일 접근을 위한 api구현

--- src/main/java/waterfogsw/highlive/controller/ApiController.java
+++ src/main/java/waterfogsw/highlive/controller/ApiController.java
@@ -9,10 +9,39 @@ import java.nio.file.Files;
 @RestController
 public class ApiController {
 
-    @RequestMapping(value = "/api/json/{id}", method = RequestMethod.GET)
-    public String getJsonFile(@PathVariable("id") String videoId) throws IOException {
+    static final String dataPath = "./python/Data/";
 
-        File resource = new File("./python/Data/result_data/"+ videoId + ".json");
+    @RequestMapping(value = "/api/traffic/{id}", method = RequestMethod.GET)
+    public String getTraffic(@PathVariable("id") String videoId) throws IOException {
+        File resource = new File(dataPath + "traffic_data/"+ videoId + ".json");
+
+        return new String(Files.readAllBytes(resource.toPath()));
+    }
+
+    @RequestMapping(value = "/api/emotion/{id}", method = RequestMethod.GET)
+    public String getEmotion(@PathVariable("id") String videoId) throws IOException {
+        File resource = new File(dataPath + "emotion_data/"+ videoId + ".json");
+
+        return new String(Files.readAllBytes(resource.toPath()));
+    }
+
+    @RequestMapping(value = "/api/highlight/{id}", method = RequestMethod.GET)
+    public String getHighlight(@PathVariable("id") String videoId) throws IOException {
+        File resource = new File(dataPath + "highlight_data/"+ videoId + ".json");
+
+        return new String(Files.readAllBytes(resource.toPath()));
+    }
+
+    @RequestMapping(value = "/api/valid/{id}", method = RequestMethod.GET)
+    public String getValid(@PathVariable("id") String videoId) throws IOException {
+        File resource = new File(dataPath + "valid_data/"+ videoId + ".json");
+
+        return new String(Files.readAllBytes(resource.toPath()));
+    }
+
+    @RequestMapping(value = "/api/raw/{id}", method = RequestMethod.GET)
+    public String getRaw(@PathVariable("id") String videoId) throws IOException {
+        File resource = new File(dataPath + "raw_data/"+ videoId + ".json");
 
         return new String(Files.readAllBytes(resource.toPath()));
     }
