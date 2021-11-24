96a40eadf0dd6619678a1477aa95f31a44c8b94c
rilac1 <rilac1@naver.com>
2021-11-24 17:29:23 +0900

    ê원형 차트 구현

--- src/main/resources/static/js/demo/chart-pie-demo.js
+++ src/main/resources/static/js/demo/chart-pie-demo.js
@@ -2,16 +2,26 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
+// getData
+var pieLabels = [];
+var pieData = [];
+$.getJSON("http://"+ window.location.host + "/api/valid/" + videoId, function(data) {
+  $.each(data, function(key, value){
+    pieLabels.push(value[0]);
+    pieData.push(parseFloat(value[1]));
+  });
+});
+
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: pieLabels,
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: pieData,
+      backgroundColor: ['#4e73df', '#1cc88a'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
