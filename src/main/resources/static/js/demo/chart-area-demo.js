65f60c74b600498b367ba5bd072f108c63f4c570
San Kim <waterfog9580@gmail.com>
2021-11-24 23:42:03 +0900

    초단위 분초로 변경

--- src/main/resources/static/js/demo/chart-area-demo.js
+++ src/main/resources/static/js/demo/chart-area-demo.js
@@ -27,6 +27,25 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   return s.join(dec);
 }
 
+function time_format(duration)
+{
+  // Hours, minutes and seconds
+  var hrs = ~~(duration / 3600);
+  var mins = ~~((duration % 3600) / 60);
+  var secs = ~~duration % 60;
+
+  // Output like "1:01" or "4:03:59" or "123:03:59"
+  var ret = "";
+
+  if (hrs > 0) {
+    ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+  }
+
+  ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+  ret += "" + secs;
+  return ret;
+}
+
 // getData
 chartLabels = [];
 chartData = [];
@@ -46,7 +65,7 @@ window.addEventListener('load', ()=> {
     data: {
       labels: chartLabels,
       datasets: [{
-        label: "Chatting Count",
+        label: "Chats",
         lineTension: 0.3,
         backgroundColor: "rgba(78, 115, 223, 0.05)",
         borderColor: "rgba(78, 115, 223, 1)",
@@ -84,7 +103,7 @@ window.addEventListener('load', ()=> {
             maxTicksLimit: 7,
             // Include a second sign
             callback: function (value, index, values) {
-              return number_format(value) + 's';
+              return time_format(value) + 's';
             }
           }
         }],
