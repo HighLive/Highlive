e8154c0f591ec445c23a1fbe06254c28fa99b4ee
San Kim <waterfog9580@gmail.com>
2021-11-24 17:42:30 +0900

    오류 수정

--- src/main/resources/static/js/demo/chart-pie-demo.js
+++ src/main/resources/static/js/demo/chart-pie-demo.js
@@ -12,40 +12,15 @@ $.getJSON("http://"+ window.location.host + "/api/valid/" + videoId, function(da
   });
 });
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: pieLabels,
-    datasets: [{
-      data: pieData,
-      backgroundColor: ['#4e73df', '#1cc88a'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-
 // 페이지가 로드 된 이후 canvas 그리기
 window.addEventListener('load', ()=> {
   var ctx = document.getElementById("myPieChart");
   var myPieChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-      labels: ["Direct", "Referral", "Social"],
+      labels: pieLabels,
       datasets: [{
-        data: [55, 30, 15],
+        data: pieData,
         backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
         hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
         hoverBorderColor: "rgba(234, 236, 244, 1)",
